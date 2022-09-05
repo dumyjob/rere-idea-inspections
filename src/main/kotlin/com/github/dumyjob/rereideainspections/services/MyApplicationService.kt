99c2e64fe3e03adb3580807512e59c52f6d5043d
GitHub Action <action@github.com>
2022-09-05 07:03:43 +0000

    Template cleanup

--- src/main/kotlin/com/github/dumyjob/rereideainspections/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/dumyjob/rereideainspections/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.dumyjob.rereideainspections.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.dumyjob.rereideainspections.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/dumyjob/rereideainspections/services/MyProjectService.kt
+++ src/main/kotlin/com/github/dumyjob/rereideainspections/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.dumyjob.rereideainspections.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.dumyjob.rereideainspections.MyBundle
 
 class MyProjectService(project: Project) {
 
